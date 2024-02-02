1b7abd3dfdb078b8767fb84c62ec9aa9fa4e35e3
Rohitash Kator <rohitashkator@Rohitashs-MacBook-Pro.local>
2024-02-02 14:15:35 +0530

    initial commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api'; // Replace with your FastAPI server URL
+// const API_BASE_URL = 'http://127.0.0.1:8000/api'; // Replace with your FastAPI server URL
+const API_BASE_URL = 'https://blog-sqf4.onrender.com/api'; // Replace with your FastAPI server URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
